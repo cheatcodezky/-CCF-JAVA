e3749e4b9b120bbf663ee664cc2ac2a059761b24
QQ951127336 <951127336@qq.com>
2017-03-18 14:10:13 +0800

    mid_number_found

--- CCF2016124_mindle_number.java
+++ CCF2016124_mindle_number.java
@@ -15,11 +15,28 @@ public class CCF2016124_mindle_number implements Comparator<Integer>{
         {
             input[i] = scanner.nextInt();
         }
-        Arrays.sort(input, new CCF2016124_mindle_number());
-        for (int i =0;i<input.length;i++)
-        {
-            System.out.print(input[i]+" ");
+        int answer = -1;
+        for (int i=0;i<number;i++) {
+            int left = 0;
+            int right =0;
+            for (int j = 0; j < number; j++) {
+                if (input[i]<input[j])
+                    right++;
+                else if(input[i] > input[j])
+                    left++;
+            }
+            if (right ==left)
+            {
+                System.out.println(input[i]);
+                return;
+            }
         }
+        System.out.println("-1");
+//        Arrays.sort(input, new CCF2016124_mindle_number());
+//        for (int i =0;i<input.length;i++)
+//        {
+//            System.out.print(input[i]+" ");
+//        }
 //        ArrayList<Integer> list= new ArrayList<Integer>(Arrays.asList(input));
 //        list.sort();
     }
